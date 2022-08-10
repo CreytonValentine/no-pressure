fb28040ef68298b518007fa3dbac0fabd91d6cf2
CreyValentine <valentine.crey95@gmail.com>
2022-08-10 08:20:06 -0500

    added question array

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// create global variables
+//I know I need an array  of questions to loop through
+var questions = [
+    {
+        prompt: "Why are WEB API's useful?\n(a) Helps with time management\n\ (b) Allows you to learn Javascript quickly\n (c) Utilizes syntax that is less complicated",
+        answer: "c"
+    },
+    {
+        prompt: "console.log() can help developers to:\n(a) Debug code with web developer tools\n\ (b) Retrieve information from the web\n (c) Store information on the web",
+        answer: "a"
+    },
+]
+
+//click event for timer button
+document.getElementById("timer").addEventListener("click", function() {
+    var timeRemaining = 60;
+})
