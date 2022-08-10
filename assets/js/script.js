fb79b22d6d61fd62f81ccb3f71a7309d7203b449
CreyValentine <valentine.crey95@gmail.com>
2022-08-10 10:43:16 -0500

    pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
-//click event for timer button
-document.addEventListener("click", function() {
-    var btn = document.createElement("button");
-    btn.textContent = "Begin Quiz!";
-    document.body.appendChild(btn);
-})
+//quiz begins by pressing button
+//quiz ends when all questions are answered or there is no time left
+//start timer and display first question when button is pressed
+    //create button
+//Use an array of multiple choice questions
+//create a loop for the questions
+//alert if question is answered correctly or incorrectly
+//add score if answer is correct and reduce score if incorrect
+
 
 //I know I need an array  of questions to loop through
 var questions = [
@@ -29,6 +32,16 @@ for(var i=0; i < questions.length; i++) {
         alert("INCORRECT!");
     }
 }
+
+document.addEventListener("click", function() {
+    var btn = document.createElement("button");
+    btn.textContent = "Begin Quiz!";
+    document.body.appendChild(btn);
+
+    btn.addEventListener("click", function() {
+        
+    })
+})
 
 
 
