3777df3df369d20cec1f2439394ac1192723999f
CreyValentine <valentine.crey95@gmail.com>
2022-08-10 12:25:04 -0500

    more pseudo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
 //quiz begins by pressing button
 //quiz ends when all questions are answered or there is no time left
 //start timer and display first question when button is pressed
-    //create button
+//create button
 //Use an array of multiple choice questions
 //create a loop for the questions
 //alert if question is answered correctly or incorrectly
 //add score if answer is correct and reduce score if incorrect
+//
+var wrongAnswer = 3;
+var correctAnswer = 2;
+var time = 60;
+var body = document.body;
+var buttonEl = document.createElement("button");
+buttonEl.textContent = "Begin Quiz";
+body.appendChild(buttonEl);
 
+//questions are appearing before button is clicked. gotta fix that
+    //attempt to create buttonEl handler and move array and loop inside?
+    //set time interval
+var beginQuiz = function(time) {
+    var startTimer = time - 1;
+    buttonEl.addEventListener("click", startTimer);
 
-//I know I need an array  of questions to loop through
+    beginQuiz();
+}
+
 var questions = [
     {
         prompt: "Why are WEB API's useful?\n(a) Helps with time management\n\ (b) Allows you to learn Javascript quickly\n (c) Utilizes syntax that is less complicated",
@@ -19,9 +35,7 @@ var questions = [
         answer: "a"
     },
 ]
-//create score variable to modify score in loop
-var score = 0
-//loop
+var score = 0;
 for(var i=0; i < questions.length; i++) {
     var answers = window.prompt(questions[i].prompt)
     if(answers == questions[i].answer) {
@@ -33,15 +47,6 @@ for(var i=0; i < questions.length; i++) {
     }
 }
 
-document.addEventListener("click", function() {
-    var btn = document.createElement("button");
-    btn.textContent = "Begin Quiz!";
-    document.body.appendChild(btn);
-
-    btn.addEventListener("click", function() {
-        
-    })
-})
 
 
 
